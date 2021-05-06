b90d509f923e7a06fc922d73c3e05a96d36b83c1
Tolgahan Tunç <tunctolgahan@gmail.com>
2021-05-06 19:10:16 +0300

    Add project files.

---
+++ WebApplication1/Models/DBConnector.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class DBConnector : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Users.db;");
+        }
+        public DbSet<User> User { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+            new User() { Id = 1, Ad = "Ali" },
+            new User() { Id = 2, Ad = "Veli" },
+            new User() { Id = 3, Ad = "Yılmaz" }
+            );
+        }
+    }
+}
